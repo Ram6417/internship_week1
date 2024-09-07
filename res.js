6c99d17ace3cd2ff7807a49fb99e63e2b224cf39
Ram6417 <tu.lakshminarayana@gmail.com>
2024-09-07 12:57:37 +0530

    Add files via upload

---
+++ res.js
@@ -0,0 +1,5 @@
+// JS can be used for additional interactivity
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Your message has been sent!');
+});
